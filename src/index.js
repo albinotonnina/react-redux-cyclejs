3bb7334cda2f7dc429902d184f22a3f3f509da1d
Albino Tonnina <albinotonnina@gmail.com>
2018-01-13 01:32:09 +0000

    💅 webfont

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import {render} from 'react-dom'
+import WebFont from 'webfontloader'
 import App from './App'
 require('babel-polyfill')
 
-render(<App foo="bar" />, document.getElementById('app'))
+const waitForWebfonts = function(fonts, callback) {
+  WebFont.load({
+    google: {
+      families: fonts
+    },
+    active: callback
+  })
+}
+
+const boot = Component => {
+  render(<Component foo="bar" />, document.getElementById('app'))
+}
+
+window.onload = waitForWebfonts(['Montserrat:400,100,300,700,900'], () => boot(App))
