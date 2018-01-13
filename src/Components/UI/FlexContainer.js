5d250aa1cb7359ced72cd45393f761f838ba0129
Albino Tonnina <albinotonnina@gmail.com>
2018-01-13 00:58:54 +0000

    🐛 scrolling was shit because of the fixed position of a before pseudo. 😳

--- src/Components/UI/FlexContainer.js
+++ src/Components/UI/FlexContainer.js
@@ -4,18 +4,6 @@ const FlexContainer = styled.div`
   display: flex;
   height: ${props => (props.height ? props.height : '100vh')};
   width: ${props => (props.width ? props.width : '100vw')};
-
-  &:before {
-    content: '';
-    background-image: url('http://c8.alamy.com/comp/DY553G/portrait-of-young-businesswoman-looking-out-of-office-window-DY553G.jpg');
-    background-size: cover;
-    filter: blur(15px);
-    height: 100%;
-    width: 100%;
-    position: fixed;
-    z-index: -1;
-    opacity: 0.4;
-  }
 `
 
 FlexContainer.displayName = 'FlexContainer'
